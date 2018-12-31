6062fe2e212f0457e178b3cda0b592188b4f059c
khuxkm fbexl <khuxkm@ttm.sh>
2018-12-31 05:29:14 -0500

    Increment version number in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='archive-cdx',  # Required
 
-    version='0.1.1',  # Required
+    version='0.2.0',  # Required
 
     description='A library to interact with the archive.org Wayback Machine CDX server.',  # Optional
 
