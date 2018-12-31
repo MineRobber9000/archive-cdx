00b566ae1862f6d43fc5aa849c7dc623a9753630
khuxkm fbexl <khuxkm@ttm.sh>
2018-12-31 05:05:01 -0500

    Split raw listing and normal listing
    Also add blah_ to blah converter so all arguments can be used.

--- archivecdx/__init__.py
+++ archivecdx/__init__.py
@@ -7,23 +7,34 @@ DONT_USE = "showResumeKey resumeKey showDupeCount showSkipCount".split()
 class Listing:
 	def __init__(self,url,delay=1,**kwargs):
 		self.url = url
-		if not "page_count" in kwargs:
-			kwargs["showNumPages"]="true"
-			self.page_count = get(url,lambda x: x,**kwargs)
-			del kwargs["showNumPages"]
-			sleep(delay)
-		else:
-			self.page_count = kwargs["page_count"]
-			del kwargs["page_count"]
-		if "page" not in kwargs: kwargs["page"]=0
-		kwargs["page"] = self.page_count if kwargs["page"]>self.page_count else kwargs["page"]
-		self.page = kwargs["page"]
+		self.delay = delay
 		self.kwargs = kwargs
+		for key in kwargs:
+			if key.endswith("_"):
+				self.kwargs[key[:-1]]=self.kwargs[key]
+				del self.kwargs[key]
+		self.pre_process()
 		for key in DONT_USE:
-			if key in kwargs: del kwargs[key]
-		self.listing = get(url,**kwargs)
+			if key in self.kwargs: del self.kwargs[key]
+		self.listing = get(self.url,**self.kwargs)
+	def pre_process(self):
+		return
 	def __getitem__(self,k):
 		return self.listing[k]
+
+class PaginatedListing(Listing):
+	def pre_process(self):
+		if not "page_count" in self.kwargs:
+			self.kwargs["showNumPages"]="true"
+			self.page_count = get(self.url,lambda x: x,**self.kwargs)
+			del self.kwargs["showNumPages"]
+			sleep(self.delay)
+		else:
+			self.page_count = self.kwargs["page_count"]
+			del self.kwargs["page_count"]
+		if "page" not in self.kwargs: self.kwargs["page"]=0
+		self.kwargs["page"] = self.page_count if self.kwargs["page"]>self.page_count else self.kwargs["page"]
+		self.page = self.kwargs["page"]
 	@property
 	def next_page(self):
 		if self.page>self.page_count:
